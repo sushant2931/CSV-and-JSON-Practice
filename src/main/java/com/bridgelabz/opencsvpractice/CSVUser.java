9622ffb94d34c984cc006a9546592adef38a88f7
sushant2931 <sushantlad8888@gmail.com>
2021-09-27 23:11:21 +0530

    ADDED: Reading From and Writing to CSV and GSON Files

---
+++ src/main/java/com/bridgelabz/opencsvpractice/CSVUser.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.opencsvpractice;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVUser {
+    @CsvBindByName
+    private String name;
+
+    @CsvBindByName(column="email", required=true)
+    private String email;
+
+    @CsvBindByName(column="phone")
+    private String phoneNo;
+
+
+    @CsvBindByName
+    private String country;
+
+    @Override
+    public String toString() {
+        return "CSVUser{name='"+ name+ "', email='"+ email+"', phoneNumber='"+ phoneNo+"', country='"+ country+"'}";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
